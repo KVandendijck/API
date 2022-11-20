2956a40efd269288eec6acc3b9be6f2f546f0a2c
KVandendijck <91118329+KVandendijck@users.noreply.github.com>
2022-11-20 13:32:27 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -10,7 +10,7 @@ origins = [
     "http://localhost:63342",
     "https://localhost.tiangolo.com/",
     "http://127.0.0.1:5500/",
-    "https://KVandendijck.github.io"
+    "https://kvandendijck.github.io"
 ]
 
 app.add_middleware(
